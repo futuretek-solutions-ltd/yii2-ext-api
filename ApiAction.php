d39eb792b5f1bc8af2b2d8c29774bb9ac0a7b3e1
Lukáš Černý <lukas.cerny.dis@gmail.com>
2015-01-04 13:54:07 +0100

    Přidání API akce

---
+++ ApiAction.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * File ApiAction.php
+ *
+ * @package ext-api
+ * @author  Lukas Cerny <lukas.cerny@futuretek.cz>
+ * @license http://www.futuretek.cz/license FTSLv1
+ * @link    http://www.futuretek.cz
+ */
+
+namespace futuretek\api;
+
+use yii\base\Action;
+
+/**
+ * Class ApiAction
+ *
+ * @package futuretek\api
+ * @author  Lukas Cerny <lukas.cerny@futuretek.cz>
+ * @license http://www.futuretek.cz/license FTSLv1
+ * @link    http://www.futuretek.cz
+ *
+ * @property ApiController $controller
+ */
+class ApiAction extends Action
+{
+    
+}
